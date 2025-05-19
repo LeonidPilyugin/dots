793d8f0c406449864dd2458806c76eb3619031ef
Leonid Pilyugin <l.pilyugin04@gmail.com>
2025-05-19 09:07:54 +0300

    chdir toggleterm

--- dots/.config/nvim/lua/config/toggleterm.lua
+++ dots/.config/nvim/lua/config/toggleterm.lua
@@ -7,5 +7,5 @@ toggleterm.setup({
     insert_mappings = false,
     shell = string.format("export TERM=%s && %s", vim.env.TERM, vim.o.shell),
     direction = "horizontal",
-    autochdir = true,
+    autochdir = false,
 })
